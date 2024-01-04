80b54af41d6d4c5af3fa1c9e286573c89d019ac2
sads4ndwich <caiocamargo007@gmail.com>
2024-01-04 01:02:14 -0300

    chore: added config const paths

---
+++ config/main.go
@@ -0,0 +1,7 @@
+package config
+
+const (
+  APACHE_SITES_ROOT         = "/etc/apache2/sites-available"
+  APACHE_SITES_DEFAULT_CONF = "/etc/apache2/sites-available/000-default.conf"
+  APACHE_PROJECT_ROOT       = "/var/www"
+)
