2af3c8767425e94333bcf99362f61164f643789a
sads4ndwich <caiocamargo007@gmail.com>
2024-01-04 14:01:39 -0300

    feat: abstract some apache commands

---
+++ lib/apache/main.go
@@ -0,0 +1,40 @@
+package apache
+
+import (
+  "os/exec"
+  "strings"
+)
+
+func Start(project string) (out []byte, err error) {
+  startCmd := exec.Command("a2ensite", project, "-q")
+  out, err = startCmd.Output()
+
+  return
+}
+
+func Stop(project string) (out []byte, err error) {
+  stopCmd := exec.Command("a2dissite", project, "-q")
+  out, err = stopCmd.Output()
+
+  return
+}
+
+func CurrentRunning() (string, error) {
+  stopCmd := exec.Command("a2query", "-s")
+  out, err := stopCmd.Output()
+
+  if err != nil { return "", err }
+
+  project := strings.Fields(string(out))[0]
+
+  return project, nil
+}
+
+func Restart() error {
+  restartCmd := exec.Command("service", "apache2", "restart")
+  _, err := restartCmd.Output()
+
+  if err != nil { return err }
+
+  return nil
+}
