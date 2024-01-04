03714d3be06668ffea76b733e7439b8ef7d1d8fa
sads4ndwich <caiocamargo007@gmail.com>
2024-01-04 14:19:30 -0300

    chore: create an build script

---
+++ scripts/build.sh
@@ -0,0 +1,14 @@
+rm -fr dist
+
+echo 'building linux-amd64...'
+GOOS=linux GOARCH=amd64 go build -o ./dist/linux-amd64/apm
+echo 'building linux-arm64...'
+GOOS=linux GOARCH=arm64 go build -o ./dist/linux-arm64/apm
+
+cd dist
+for dir in $(ls -d *); do
+    tar cfzv "$dir".tgz $dir
+    rm -rf $dir
+done
+
+echo 'done!'
