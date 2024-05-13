3ef1ae0553999a49919fab207cf9b74ad01eeee9
sads4ndwich <caiocamargo007@gmail.com>
2024-05-13 20:46:35 -0300

    chore: remove old file

--- config/main.go
+++
@@ -1,7 +0,0 @@
-package config
-
-const (
-  APACHE_SITES_ROOT         = "/etc/apache2/sites-available"
-  APACHE_SITES_DEFAULT_CONF = "/etc/apache2/sites-available/000-default.conf"
-  APACHE_PROJECT_ROOT       = "/var/www"
-)
