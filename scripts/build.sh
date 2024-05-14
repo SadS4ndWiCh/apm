1c51e624a67d1b47d4870f5fdd7d72c3695f391c
sads4ndwich <caiocamargo007@gmail.com>
2024-05-13 21:58:27 -0300

    fix: build command

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,12 @@
 rm -fr dist
+cd cmd/cli
 
 echo 'building linux-amd64...'
-GOOS=linux GOARCH=amd64 go build -o ./dist/linux-amd64/apm
+GOOS=linux GOARCH=amd64 go build -o ../../dist/linux-amd64/apm
 echo 'building linux-arm64...'
-GOOS=linux GOARCH=arm64 go build -o ./dist/linux-arm64/apm
+GOOS=linux GOARCH=arm64 go build -o ../../dist/linux-arm64/apm
 
-cd dist
+cd ../../dist
 for dir in $(ls -d *); do
     tar cfzv "$dir".tgz $dir
     rm -rf $dir
